e5564179664375762210f6f8e7d1d5712c9e291f
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-12-11 23:32:37 +0100

    dissect: Print the header of the actual update format

--- update-ec20/dissect.py
+++ update-ec20/dissect.py
@@ -56,9 +56,44 @@ lzma now?
         print("No fs type update with type: 0x%.2x" % upd[12])
         return 0
     assert fs_hdr == upd[0:len(fs_hdr)]
-    upd_hdr = upd[len(fs_hdr):64]
-    upd_dat = upd[64:]
-    print(hexstring(upd_hdr))
+    rstr = io.BytesIO(upd[len(fs_hdr):])
+    ram_size = struct.unpack("<I", rstr.read(4))[0]
+    sec_size = struct.unpack("<I", rstr.read(4))[0]
+    dic_size = struct.unpack("<I", rstr.read(4))[0]
+    com_size = struct.unpack("<I", rstr.read(4))[0]
+    min_allo = struct.unpack("<H", rstr.read(2))[0]
+    inf_size = struct.unpack("<H", rstr.read(2))[0]
+    num_copy = struct.unpack("<H", rstr.read(2))[0]
+    num_diff = struct.unpack("<H", rstr.read(2))[0]
+    num_inse = struct.unpack("<H", rstr.read(2))[0]
+    num_dele = struct.unpack("<H", rstr.read(2))[0]
+    num_ddir = struct.unpack("<H", rstr.read(2))[0]
+    num_dirs = struct.unpack("<H", rstr.read(2))[0]
+    num_dlnk = struct.unpack("<H", rstr.read(2))[0]
+    num_link = struct.unpack("<H", rstr.read(2))[0]
+    num_cupd = struct.unpack("<H", rstr.read(2))[0]
+    num_cins = struct.unpack("<H", rstr.read(2))[0]
+    print("Delta Info: delta_size - %d" % (len(upd)+8))
+    print("Delta Info: ver - ???")
+    print("Delta Info: scout_ver - ???")
+    print("Delta Info: flags - ???")
+    print("Delta Info: runtype_flags - ???")
+    print("Delta Info: ram_size - 0x%x" % ram_size)
+    print("Delta Info: sector_size - 0x%x" % sec_size)
+    print("Delta Info: dic_sz - 0x%x" % dic_size)
+    print("Delta Info: compress_sz - 0x%x" % com_size)
+    print("Delta Info: min_alloc_ram_use - 0x%x" % min_allo)
+    print("Delta Info: ext_info_sz - %d" % inf_size)
+    print("Delta Info: num_copy - %d" % num_copy)
+    print("Delta Info: num_diff - %d" % num_diff)
+    print("Delta Info: num_insert - %d" % num_inse)
+    print("Delta Info: num_delete - %d" % num_dele)
+    print("Delta Info: num_del_dirs - %d" % num_ddir)
+    print("Delta Info: num_dirs - %d" % num_dirs)
+    print("Delta Info: num_del_link - %d" % num_dlnk)
+    print("Delta Info: num_link - %d" % num_link)
+    print("Delta Info: num_critical_update - %d" % num_cupd)
+    print("Delta Info: num_critical_insert - %d" % num_cins)
     #print(hexstring(upd_dat))
 
 # Parse the chunk..
