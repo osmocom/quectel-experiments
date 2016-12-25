03c523884ea52e56198c9ea96316e84b0e53a27b
Harald Welte <laforge@gnumonks.org>
2016-12-25 17:22:20 +0100

    qmi_test: some more debug output so it can be correlated with qmuxd traces

--- qmi/qmi_test.c
+++ qmi/qmi_test.c
@@ -17,21 +17,25 @@ int main(int argc, char **argv)
 	uint32_t num_services, num_entries = 0;
 	int rc;
 
+	printf("== QMI Init...\n");
 	g_qmi_handle = qmi_init(NULL, NULL);
 	if (g_qmi_handle < 0) {
 		fprintf(stderr, "qmi_init() failed: %d\n", g_qmi_handle);
 		exit(1);
 	}
 
+	printf("== DMS Get Service Object...\n");
 	dm_service_obj = dms_get_service_object_v01();
 	if (!dm_service_obj) {
 		fprintf(stderr, "Cannot get service object\n");
 		return -1;
 	}
+	printf("== Client Notifier Init...\n");
 	rc = qmi_client_notifier_init(dm_service_obj, &os_params, &notifier);
 
 	/* wait for service and get number of services */
 	while (1) {
+		printf("Get Service List...\n");
 		rc = qmi_client_get_service_list(dm_service_obj, NULL, NULL, &num_services);
 		if (rc == QMI_NO_ERR)
 			break;
@@ -39,24 +43,26 @@ int main(int argc, char **argv)
 		QMI_CCI_OS_SIGNAL_WAIT(&os_params, 0);
 	}
 	num_entries = num_services;
-	printf("%u services available\n", num_services);
+	printf("== %u services available\n", num_services);
 
 	/* obtain service info */
 	rc = qmi_client_get_service_list(dm_service_obj, info, &num_entries, &num_services);
-	printf("qmi_client_get_service_list() returned %d num_entries = %d num_services = %d\n", rc, num_entries, num_services);
+	printf("== aqmi_client_get_service_list() returned %d num_entries = %d num_services = %d\n", rc, num_entries, num_services);
 
 	rc = qmi_client_init(&info[0], dm_service_obj, NULL, NULL, NULL, &clnt);
-	printf("qmi_client_init() returned %d\n", rc);
+	printf("== qmi_client_init() returned %d\n", rc);
 
 	/* FIXME: main */
 	dms_get_device_serial_numbers_resp_msg_v01 serno_resp;
 
 	rc = qmi_client_send_msg_sync(clnt, QMI_DMS_GET_DEVICE_SERIAL_NUMBERS_REQ_V01, NULL, 0, &serno_resp, sizeof(serno_resp), 0);
-	printf("qmi_client_send_msg_sync() returned %d\n", rc);
-	printf("IMEI IS %s\n", serno_resp.imei_valid ? serno_resp.imei : "invalid");
+	printf("== qmi_client_send_msg_sync() returned %d\n", rc);
+	printf("== IMEI IS %s\n", serno_resp.imei_valid ? serno_resp.imei : "invalid");
 
 	/* clean-up */
+	printf("== qmi_client_release(clnt)\n");
 	rc = qmi_client_release(clnt);
+	printf("== qmi_client_release(notifier)\n");
 	rc = qmi_client_release(notifier);
 	sleep(1);
 	exit(0);
